cf61409bd0fe734027d1f9ff1adbaecfd71bd48d
B R Pawan Kumar Iyengar <pawanku1@in.ibm.com>
2019-02-16 15:17:22 +0530

    Automate Calculator Functions
    The application automates calculator functions using screenshots of the buttons on the Windows 10 system (standard calculator).

---
+++ Calculator_Automation.py
@@ -0,0 +1,13 @@
+#We import the required packages Pyautogui and time
+import pyautogui as Pawan_Robo_Program
+import time as Samay_in_Seconds
+
+Samay_in_Seconds.sleep(3)
+
+Pawan_Robo_Program.click((Pawan_Robo_Program.locateCenterOnScreen('Number_4.png')))
+Samay_in_Seconds.sleep(1)
+Pawan_Robo_Program.click((Pawan_Robo_Program.locateCenterOnScreen('Number_Multiply.png')))
+Samay_in_Seconds.sleep(1)
+Pawan_Robo_Program.click((Pawan_Robo_Program.locateCenterOnScreen('Number_9.png')))
+Samay_in_Seconds.sleep(1)
+Pawan_Robo_Program.press('enter')
