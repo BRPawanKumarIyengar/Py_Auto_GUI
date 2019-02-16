897b5ac56cd03435942f7be1c77017619771bb04
B R Pawan Kumar Iyengar <pawanku1@in.ibm.com>
2019-02-16 21:17:52 +0530

    Update Calculator_Automation.py
    Added few comments

--- Calculator_Automation.py
+++ Calculator_Automation.py
@@ -2,8 +2,10 @@
 import pyautogui as Pawan_Robo_Program
 import time as Samay_in_Seconds
 
+#Here we give ourselves three seconds to open calculator app
 Samay_in_Seconds.sleep(3)
 
+#Here we use the 'locateCenterOnScreen'  function to detect the calculator buttons and 'Click' dfunction to click the buttons
 Pawan_Robo_Program.click((Pawan_Robo_Program.locateCenterOnScreen('Number_4.png')))
 Samay_in_Seconds.sleep(1)
 Pawan_Robo_Program.click((Pawan_Robo_Program.locateCenterOnScreen('Number_Multiply.png')))
